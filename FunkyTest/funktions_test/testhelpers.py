69bb1b66c4730a6df35f1b5c137cd0a9c18efdee
James Niedfeldt <jamesniedfeldt@gmail.com>
2020-02-05 12:24:21 -0700

    Finished tests for helpers

--- FunkyTest/funktions_test/testhelpers.py
+++ FunkyTest/funktions_test/testhelpers.py
@@ -4,10 +4,12 @@ import datetime
 import os
 from funktions_test import mockmessage as m
 from funktions import helpers as h
+from reminder import database as db
+from reminder import reminder as r
 
 class TestHelpers(unittest.TestCase):
     @classmethod
-    def setUpClass(self):       
+    def setUpClass(self):
         self.msg = m.MockMessage()
         self.msg.server = "test server"
         self.msg.timestamp = datetime.datetime(2000,1,1)
@@ -64,8 +66,62 @@ class TestHelpers(unittest.TestCase):
         #Nothing to test
         pass
 
-    
+    def test_formatTime(self):
+        string = "0 days, 0 hours, 0 minutes, and 0 seconds"
+        self.assertEqual(h.formatTime(0), string)
+        
+        string = "0 days, 0 hours, 0 minutes, and 30 seconds"
+        self.assertEqual(h.formatTime(30), string)
 
+        string = "0 days, 0 hours, 1 minutes, and 0 seconds"
+        self.assertEqual(h.formatTime(60), string)
+
+        string = "0 days, 1 hours, 0 minutes, and 0 seconds"
+        self.assertEqual(h.formatTime(3600), string)
+
+        string = "1 days, 0 hours, 0 minutes, and 0 seconds"
+        self.assertEqual(h.formatTime(86400), string)
+
+        string = "2 days, 2 hours, 2 minutes, and 2 seconds"
+        self.assertEqual(h.formatTime(180122), string)
+
+        string = "0 days, 0 hours, 0 minutes, and 0 seconds"
+        self.assertEqual(h.formatTime(30, 30), string)
+
+        string = "0 days, 0 hours, 0 minutes, and 30 seconds"
+        self.assertEqual(h.formatTime(60, 30), string)
+
+        string = "0 days, 0 hours, 1 minutes, and 0 seconds"
+        self.assertEqual(h.formatTime(90, 30), string)
+
+        string = "Error: Invalid time"
+        self.assertEqual(h.formatTime(30, 31), string)
+
+        string = "Error: Invalid time"
+        self.assertEqual(h.formatTime(-30), string)
+
+    def test_filePath(self):
+        head = os.path.split(
+            os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+        abspath = head[0] + "/FunkyBot"
+        
+        path = os.path.join(abspath, "test_directory")
+        self.assertEqual(h.filePath("test_directory"), path)
+
+        path = os.path.join(abspath, "")
+        self.assertEqual(h.filePath(""), path)
+
+    def test_badArgs(self):
+        msg = "I couldn't understand your command! If you need help, send `!help [[!test]]`."
+        self.assertEqual(h.badArgs("test"), msg)
+
+        msg = "I couldn't understand your command! If you need help, send `!help [[!]]`."
+        self.assertEqual(h.badArgs(""), msg)
+
+    def test_setUpReminders(self):
+        #Nothing to test
+        pass
+    
     def tearDown(self):
         if(os.path.isfile(self.logpath)):
             os.remove(self.logpath)
